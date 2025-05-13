be9c9f38330035fdfb3870928a7fe99b1a19a1a6
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2025-05-13 13:57:55 +0100

    Initial project setup and configuration

---
+++ src/test/java/com/example/spring_cloud_config_server_client_implementation/SpringCloudConfigServerClientImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_config_server_client_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerClientImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
